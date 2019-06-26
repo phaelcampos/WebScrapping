7fbcbfc0cd817456cc89b423cfba0f0b0c015471
Raphael Campos <raphelcamachado@gmail.com>
2019-06-26 15:22:11 -0300

    A simple web scrapping file

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 if __name__ == "__main__":
     print("hello world")
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+res = requests.get("http://www.nationmaster.com/country-info/stats/Media/Internet-users")
+soup = BeautifulSoup(res.content, 'lxml')
+table = soup.find_all('table')[0]
+df = pd.read_html(str(table))
+print(tabulate(df[0], headers='keys', tablefmt='psql'))
