ec648ed59825495267c2a0482b4cf39b7e91540a
Raphael Campos <raphelcamachado@gmail.com>
2019-06-28 13:49:24 -0300

    creating a json file, with a dict that filters the search

--- parsedata.py
+++ parsedata.py
@@ -2,7 +2,11 @@ import json
 with open('twitterData.json') as json_data:
     jsonData = json.load(json_data)
 
+word=[]
 for i in jsonData:
-    if "black" in i['tweet']:
-        print (i['tweet'])
-        print(i['date'])
+    if "Friend" in i['tweet']:
+        word.append(i['date'])
+        word.append(i['likes'])
+
+with open('wordData.json', 'w') as outfile: #creating a Json file to put all the data that was filtered
+    json.dump(word, outfile)
