3d6aa44887a468dde31b3153f77557a58c319ad8
phaelcampos <30702104+phaelcampos@users.noreply.github.com>
2019-07-29 16:55:23 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,30 @@ if __name__ == "__main__":
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from tabulate import tabulate
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-res = requests.get("http://www.nationmaster.com/country-info/stats/Media/Internet-users")
-soup = BeautifulSoup(res.content, 'lxml')
-table = soup.find_all('table')[0]
-df = pd.read_html(str(table))
-print(tabulate(df[0], headers='keys', tablefmt='psql'))
+
+
+
+def scrape_stats(base_url, year_start, year_end):
+    years = range(year_start,year_end+1,1)
+
+    final_df = pd.DataFrame()
+
+    for year in years:
+        print('Extraindo ano {}'.format(year))
+        req_url = base_url.format(year)
+        req = requests.get(req_url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        table = soup.find('table', {'id':'totals_stats'})
+        df = pd.read_html(str(table))[0]
+        df['Year'] = year
+
+        final_df = final_df.append(df)
+
+    return final_df
+
+url = 'https://www.basketball-reference.com/leagues/NBA_{}_totals.html'
+
+df = scrape_stats(url, 2013, 2014)
